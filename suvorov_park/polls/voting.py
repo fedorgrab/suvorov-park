a755d383b59567d61671a540d105c8546d2f3f13
Fedor Grab <fvgrab@gmail.com>
2019-09-01 17:08:16 +0300

    Fix(polls): Implement Inappropriate choice for poll handling

--- suvorov_park/polls/voting.py
+++ suvorov_park/polls/voting.py
@@ -2,13 +2,19 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
-from suvorov_park.polls.models import Choice, UserChoice
+from suvorov_park.polls.exceptions import IncorrectPollChoice
+from suvorov_park.polls.models import Choice, Vote
 
 
-def vote(choice_id):
-    Choice.objects.filter(id=choice_id).update(votes=models.F("votes") + 1)
+def vote(choice_id, poll_id):
+    updated = Choice.objects.filter(id=choice_id, poll_id=poll_id).update(
+        votes=models.F("votes") + 1
+    )
+
+    if not updated:
+        raise IncorrectPollChoice
 
 
-@receiver(pre_save, sender=UserChoice)
+@receiver(pre_save, sender=Vote)
 def vote_for_poll_choice(sender, instance, **kwargs):
-    vote(instance.choice_id)
+    vote(choice_id=instance.choice_id, poll_id=instance.poll_id)
