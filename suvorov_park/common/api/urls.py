405311698715dcd382610e6d03546102eb2025f0
Fedor Grab <fvgrab@gmail.com>
2019-09-03 23:25:24 +0300

    Feat(site_settings): Implement Site config API

--- suvorov_park/common/api/urls.py
+++ suvorov_park/common/api/urls.py
@@ -4,4 +4,7 @@ from suvorov_park.common.api import views
 
 app_name = "common"
 
-urlpatterns = [path("news", views.NewsListAPIView.as_view(), name="news")]
+urlpatterns = [
+    path("general-configuration", views.SettingAPIView.as_view(), name="genera_config"),
+    path("news", views.NewsListAPIView.as_view(), name="news"),
+]
